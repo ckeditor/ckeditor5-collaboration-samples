eace033626a77bee3439b0861cc0ae9b22e5d2f1
f1ames <k.krzton@cksource.com>
2024-08-07 12:33:18 +0200

    Internal: Updated dependencies.

--- real-time-collaboration-for-vue/src/main.js
+++ real-time-collaboration-for-vue/src/main.js
@@ -6,6 +6,6 @@
 import { createApp } from 'vue';
 import './style.css';
 import App from './App.vue';
-import CKEditor from '@ckeditor/ckeditor5-vue';
+import { CkeditorPlugin } from '@ckeditor/ckeditor5-vue';
 
-createApp( App ).use( CKEditor ).mount( '#app' );
+createApp( App ).use( CkeditorPlugin ).mount( '#app' );
