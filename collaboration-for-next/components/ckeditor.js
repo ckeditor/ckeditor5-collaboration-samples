85c94211931358ffa4c3a866137261921b45a848
Paweł Smyrek <p.smyrek@cksource.com>
2026-07-01 12:00:50 +0200

    Internal: Updated dependencies.

--- collaboration-for-next/components/ckeditor.js
+++ collaboration-for-next/components/ckeditor.js
@@ -302,8 +302,8 @@ function CKEditorComponent() {
 		},
 		exportPdf: {
 			stylesheets: [
-				'https://cdn.ckeditor.com/ckeditor5/48.2.0/ckeditor5.css',
-				'https://cdn.ckeditor.com/ckeditor5-premium-features/48.2.0/ckeditor5-premium-features.css'
+				'https://cdn.ckeditor.com/ckeditor5/48.3.0/ckeditor5.css',
+				'https://cdn.ckeditor.com/ckeditor5-premium-features/48.3.0/ckeditor5-premium-features.css'
 			],
 			fileName: 'export-pdf-demo.pdf',
 			appID: 'cke5-demos',
@@ -322,8 +322,8 @@ function CKEditorComponent() {
 		},
 		exportWord: {
 			stylesheets: [
-				'https://cdn.ckeditor.com/ckeditor5/48.2.0/ckeditor5.css',
-				'https://cdn.ckeditor.com/ckeditor5-premium-features/48.2.0/ckeditor5-premium-features.css'
+				'https://cdn.ckeditor.com/ckeditor5/48.3.0/ckeditor5.css',
+				'https://cdn.ckeditor.com/ckeditor5-premium-features/48.3.0/ckeditor5-premium-features.css'
 			],
 			fileName: 'export-word-demo.docx',
 			converterOptions: {
